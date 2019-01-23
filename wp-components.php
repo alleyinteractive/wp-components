d5e491e2b0924b870408622bfa460660484f3d19
James Burke <james.walter.burke@gmail.com>
2019-01-23 12:30:48 -0600

    Setup autoloaders

--- wp-components.php
+++ wp-components.php
@@ -23,48 +23,8 @@ define( 'WP_COMPONENTS_PATH', dirname( __FILE__ ) );
  */
 define( 'WP_COMPONENTS_PHP_ASSET_PATH', get_stylesheet_directory() . '/client/build' );
 
-// Load template tag helpers.
+// Include autoloaders.
+require_once 'autoload.php';
+
+// Load WP_Render template tags.
 require_once 'inc/wp-render/template-tags.php';
-
-/**
- * Autoloader.
- */
-spl_autoload_register(
-	function( $class ) {
-		$class = ltrim( $class, '\\' );
-		if ( false !== strpos( $class, 'WP_Components' ) ) {
-			// echo $class;
-			// echo "</br>";
-
-			/**
-			 * Strip the namespace, replace underscores with dashes, and lowercase.
-			 *
-			 * `\WP_Component\Component\Slim_Navigation\Menu`
-			 * becomes
-			 * `slim-navigation\class-menu.php`
-			 */
-			$class = strtolower(
-				str_replace(
-					[ 'WP_Components\\Component\\', '_' ],
-					[ '', '-' ],
-					$class
-				)
-			);
-
-			$dirs  = explode( '\\', $class );
-			$class = array_pop( $dirs );
-
-			// Check if this is a class.
-			$class_path  = WP_COMPONENTS_PATH . rtrim( '/inc/' . implode( '/', $dirs ), '/' ) . "/classes/class-{$class}.php";
-			if ( file_exists( $class_path ) ) {
-				require_once $class_path;
-			}
-
-			// Check if this is a trait.
-			$trait_path  = WP_COMPONENTS_PATH . rtrim( '/inc/' . implode( '/', $dirs ), '/' ) . "/traits/trait-{$class}.php";
-			if ( file_exists( $trait_path ) ) {
-				require_once $trait_path;
-			}
-		}
-	}
-);
