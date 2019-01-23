7a80c439419f1e689ebb149d8809c756cafb936e
James Burke <james.walter.burke@gmail.com>
2019-01-23 18:51:53 -0500

    Update wp-components-php/inc/classes/class-renderable.php
    Co-Authored-By: ostowe <ostowe@gmail.com>

--- wp-components-php/inc/classes/class-renderable.php
+++ wp-components-php/inc/classes/class-renderable.php
@@ -102,7 +102,7 @@ class Renderable {
 	}
 
 	/**
-	 * Resolve and render a component's CSS
+	 * Resolve and render a component's CSS.
 	 */
 	public function render_css() {
 		$name = $this->component_instance->name;
