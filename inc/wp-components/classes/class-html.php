f7d86c5c8fe8694427b0649cfd4187831ab2f403
James Burke <james.walter.burke@gmail.com>
2019-02-13 15:36:57 -0600

    Add HTML component

---
+++ inc/wp-components/classes/class-html.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * HTML component.
+ *
+ * @package WP_Components
+ */
+
+namespace WP_Components;
+
+/**
+ * HTML.
+ */
+class HTML extends Component {
+
+	/**
+	 * Unique component slug.
+	 *
+	 * @var string
+	 */
+	public $name = 'html';
+
+	/**
+	 * Define the default config of a header.
+	 *
+	 * @return array
+	 */
+	public function default_config() {
+		return [
+			'content' => '',
+		];
+	}
+}
