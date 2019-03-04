b5b851ac0d1ecfd579ed0b96c90348e7e09d9ad2
Owen Stowe <ostowe@gmail.com>
2019-03-04 18:22:58 -0500

    Add logic for merging static block content
    - Also manage innerBlocks

--- inc/wp-components/classes/class-gutenberg-content.php
+++ inc/wp-components/classes/class-gutenberg-content.php
@@ -39,7 +39,6 @@ class Gutenberg_Content extends Component {
 						apply_filters( 'the_content', $this->post->post_content ?? '' )
 					)
 			);
-
 		} else {
 			$blocks = (array) parse_blocks( $this->post->post_content ?? '' );
 
@@ -55,53 +54,47 @@ class Gutenberg_Content extends Component {
 				)
 			);
 
-			$blocks_as_components = array_map( [ $this, 'convert_block_to_component' ], $blocks );
+			$blocks_as_components = array_reduce( $blocks, [ $this, 'convert_block_to_component' ], [] );
 			$this->append_children( $blocks_as_components );
 		}
 
 		return $this;
 	}
 
-
 	/**
 	 * Map a block array to a Component instance.
 	 *
 	 * @todo: Create "columns" and "column" components that can handle placing child blocks within the wrapping markup.
 	 *
-	 * @param array $block A parsed block associative array.
+	 * @param array $blocks         Accumulated array of blocks.
+	 * @param array $current_block  Current block.
 	 * @return object Component instance
 	 */
-	private function convert_block_to_component( $block ) : object {
-
-		$block = (array) $block;
+	private function convert_block_to_component( $blocks, $current_block ) : array {
+		$block = (array) $current_block;
 
 		// Handle gutenberg embeds.
 		if ( strpos( $block['blockName'] ?? '', 'core-embed' ) === 0 ) {
-			return ( new Blocks\Core_Embed() )->set_from_block( $block );
+			$blocks[] = ( new Blocks\Core_Embed() )->set_from_block( $block );
+			return $blocks;
 		}
 
 		// The presence of html means this is a non dynamic block.
 		if ( ! empty( $block['innerHTML'] ) ) {
-			$content = $block['innerHTML'];
+			$content = $this->get_block_html_content( $block );
+			$last_block = end( $blocks );
 
-			// Missing blockName means it's a "classic" block, run the_content.
-			if ( empty( $block['blockName'] ) ) {
-				$content = apply_filters( 'the_content', $content );
+			// Merge non-dynamic block content into a single HTML component.
+			if ( $last_block instanceof HTML ) {
+				$last_block->set_config(
+					'content',
+					$last_block->get_config( 'content' ) . $content
+				);
+			} else {
+				$blocks[] = ( new HTML() )->set_config( 'content', $content );
 			}
 
-			// Clean up extraneous whitespace characters.
-			$content = preg_replace( '/[\r\n\t\f\v]/', '', $content );
-
-			// Handle nested blocks.
-			$children_blocks_as_components = array_map(
-				[ $this, 'convert_block_to_component' ],
-				(array) ( $block['innerBlocks'] ?? [] )
-			);
-
-			return ( new HTML() )
-				->merge_config( $block['attrs'] ?? [] )
-				->set_config( 'content', $content )
-				->append_children( $children_blocks_as_components );
+			return $blocks;
 		}
 
 		// Handle nested blocks.
@@ -111,9 +104,48 @@ class Gutenberg_Content extends Component {
 		);
 
 		// A dynamic block. All attributes will be available.
-		( new Component() )
+		$blocks[] = ( new Component() )
 			->set_name( $block['blockName'] ?? '' )
 			->merge_config( $block['attrs'] ?? [] )
 			->append_children( $children_blocks_as_components );
+
+		return $blocks;
+	}
+
+	/**
+	 * Map a block array to a Component instance.
+	 *
+	 * @todo: Create "columns" and "column" components that can handle placing child blocks within the wrapping markup.
+	 *
+	 * @param array $block Gutenberg block from which to retrieve HTMl content.
+	 * @return string HTMl content of gutenberg block.
+	 */
+	public function get_block_html_content( $block ) : string {
+		$content = '';
+		$inner_blocks = $block['innerBlocks'];
+
+		// Loop through inner content if it's not empty.
+		if ( ! empty( $block['innerContent'] ) ) {
+			foreach ( $block['innerContent'] as $inner_content ) {
+				// Add inner content item if it's not empty, otherwise add content of inner blocks.
+				if ( ! empty( $inner_content ) ) {
+					$content .= $inner_content;
+				} else if ( ! empty( $inner_blocks ) ) {
+					$content .= $this->get_block_html_content( array_shift( $inner_blocks ) );
+				}
+			}
+		} else {
+			$content = $block['innerHTML'];
+		}
+
+		// Missing blockName means it's a "classic" block, run the_content.
+		if ( empty( $block['blockName'] ) ) {
+			$content = apply_filters( 'the_content', $content );
+		}
+
+		// Clean up extraneous whitespace characters.
+		$content = preg_replace( '/[\r\n\t\f\v]/', '', $content );
+
+		return $content;
 	}
 }
