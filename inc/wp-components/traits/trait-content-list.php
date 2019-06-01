091d0e44d9585f0d7f74acbdc4bf3f413530e3b9
James Burke <james.walter.burke@gmail.com>
2019-06-01 13:18:15 -0400

    Add backfill method

--- inc/wp-components/traits/trait-content-list.php
+++ inc/wp-components/traits/trait-content-list.php
@@ -121,4 +121,44 @@ trait Content_List {
 
 		return $this;
 	}
+
+	/**
+	 * Backfill an array of post ids.
+	 *
+	 * @param array   $content_item_ids Array of post ids.
+	 * @param integer $backfill_to      Amount of content needed.
+	 * @param array   $backfill_args    Arguments for WP_Query.
+	 * @return array
+	 */
+	public function backfill_content_item_ids( array $content_item_ids = [], $backfill_to = 0, $backfill_args = [] ) {
+
+		// Backfill is disabled, or unnecessary.
+		if ( 0 === $backfill_to || $backfill_to <= count( $content_item_ids ) ) {
+			return $content_item_ids;
+		}
+
+		// Modify backfill args.
+		$backfill_args['post__not_in']   = $content_item_ids;
+		$backfill_args['posts_per_page'] = $backfill_to - count( $content_item_ids );
+		$backfill_args['fields']         = 'ids';
+
+		$backfill_query = $this->get_backfill_wp_query( $backfill_args );
+
+		if ( ! empty( $backfill_query->posts ?? [] ) ) {
+			$content_item_ids = array_merge( $content_item_ids, $backfill_query->posts );
+		}
+
+		return $content_item_ids;
+	}
+
+	/**
+	 * Wrapper method for executing a backfill query. Allows for easy
+	 * overriding.
+	 *
+	 * @param array $args \WP_Query args.
+	 * @return mixed WP_Query object (or something that works like WP_Query).
+	 */
+	public function get_backfill_wp_query( $backfill_args ) {
+		return new \WP_Query( $backfill_args );
+	}
 }
