6184850c237a4509dbe28f901f9c3360936b2ed5
cahdeemer <chrstina@alley.co>
2019-09-25 11:58:00 -0400

    add heading helper

---
+++ inc/wp-components/classes/helpers/class-heading.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Heading component. This helper is a generic heading to be used anywhere.
+ *
+ * @package WP_Components.
+ */
+
+namespace WP_Components\Helpers;
+
+/**
+ * Class for a heading.
+ */
+class Heading extends \WP_Components\Component {
+
+	/**
+	 * Unique component slug.
+	 *
+	 * @var string
+	 */
+	public $name = 'heading';
+
+	/**
+	 * Define a default config.
+	 *
+	 * @return array Default config.
+	 */
+	public function default_config() : array {
+		return [
+			'Component'  => '',
+			'class_name' => '',
+			'link'       => '',
+			'type_style' => '',
+		];
+	}
+}
