85a4903587489639b377a07ca8afa85f87a61194
Renato Alves <renato@alley.co>
2019-10-21 23:09:01 -0300

    Making travis happy

--- inc/wp-components/classes/class-pagination.php
+++ inc/wp-components/classes/class-pagination.php
@@ -51,7 +51,6 @@ class Pagination extends Component {
 
 			foreach ( $pagination_links as $link_html ) {
 				$this->append_child(
-
 					// Create a new pagination item using anchor HTML, and remove various url params.
 					( new Pagination_Item() )
 						->set_from_html( $link_html )
